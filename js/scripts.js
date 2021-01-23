f83409209a7ebd39a417199b770687959532f94f
Alan Contreras <alanrcontrerasm@gmail.com>
2021-01-22 23:40:40 -0800

    Add javascript functionality

--- js/scripts.js
+++ js/scripts.js
@@ -3,80 +3,110 @@ console.log("Building a basic rock paper scissors game");
 
 //select option divs and create Event listeners
 
-let optionDivs = document.querySelectorAll(".options");
+const optionDivs = document.querySelectorAll(".option");
+const playerScoreDiv = document.querySelector("#playerS");
+const computerScoreDiv = document.querySelector("#computerS");
+const resultDiv = document.querySelector("#result");
+let computerScore = 0;
+let playerScore = 0;
+
+
+
+startNewGame();
+
+function startNewGame(){
+
+    //reactivating event listeners on options
+    optionDivs.forEach( button =>{
+        button.addEventListener("click", playRound);
+    })
+
+    //resetting score
+    computerScore = 0;
+    playerScore = 0;
+
+    //removing click event listener from resultDiv
+    resultDiv.removeEventListener("click", startNewGame);
+    resultDiv.textContent = "First to reach 5 wins is the CHAMP";
+
+    //removing animations for clicking resultDiv
+    resultDiv.classList.remove("option");
+}
 
 
 
 
+//we compare player selection to computer
+//and we return a string indicating who won
+//by default we call the functions if no values are sent
+function playRound(e){
+    const computerSelection = getComputerPlay()
+    const playerSelection = e.target.textContent.toLowerCase();
 
+    //checks if we already reached a champ
+    if(computerScore == 5 || playerScore ==5 ){
+        //After we are done playing remove event listeners
+        optionDivs.forEach( button =>{
+        button.removeEventListener("click", playRound);
+        })
 
+        //Ask if we want to play again
+        resultDiv.textContent = "No More Rounds, Click Me to Start Over"
+        resultDiv.addEventListener("click", startNewGame);
+        //adding animations for clicking
+        resultDiv.classList.toggle("option");
 
+    }else if(playerSelection == computerSelection){
 
+        resultDiv.textContent = `It's a Tie! ${playerSelection} vs ${computerSelection}`;
+        
+    }else if((playerSelection == "rock" && computerSelection == "scissors")
+        || (playerSelection == "paper" && computerSelection == "rock")
+        || (playerSelection == "scissors" && computerSelection == "paper")){
+
+            resultDiv.textContent = `Player Wins! ${playerSelection} beats ${computerSelection}`
+        playerScore++;
+
+    }else{
+        resultDiv.textContent = `Player Loses! ${playerSelection} loses against ${computerSelection}`;
+        computerScore++;
+        
+    }
+    
+    updateScore();
+    
+}
 
 //create computerPlay function no params
 //returns either rock paper or scissors lowercase
 function getComputerPlay(){
-
+    
     switch(Math.ceil(Math.random()*3)){
         case 1:
             return "rock";
             //break; not needed when using return
         case 2:
             return "paper";
-        case 3:
-            return "scissors";
-        default:
-            console.log("Error on math random");
+            case 3:
+                return "scissors";
+                default:
+                    console.log("Error on math random");
             break;
-
+            
     }
 
 }
 
-// //just testing if we get those three options
-// for(let i = 0; i< 10; i++){
-//     console.log(getComputerPlay());
-// }
 
-
-//we do a prompt to ask user for selection
-function getPlayerSelection(){
-    let playerSelection = "";
-
-    playerSelection = prompt("Choose either rock, paper or scissors >>");
-
-
-    return playerSelection.toLowerCase().trim();
+function updateScore(){
+    playerScoreDiv.innerHTML = `<h1>Player Score:<br><strong>${playerScore}</strong></h1>`;
+    computerScoreDiv.innerHTML = `<h1>Computer Score:<br><strong>${computerScore}</strong></h1>`;
 }
 
-// console.log(getPlayerSelection());
 
-
-//we compare player selection to computer
-//and we return a string indicating who won
-//by default we call the functions if no values are sent
-function playRound(playerSelection = getPlayerSelection(), 
-                    computerSelection = getComputerPlay()){
-    
-
-    if(playerSelection == computerSelection){
-
-        console.log(`It's a Tie! ${playerSelection} vs ${computerSelection}`)
-        return 0;
-
-    }else if((playerSelection == "rock" && computerSelection == "scissors")
-        || (playerSelection == "paper" && computerSelection == "rock")
-        || (playerSelection == "scissors" && computerSelection == "paper")){
-
-        console.log(`Player Wins! ${playerSelection} beats ${computerSelection}`)
-        return 1;
-
-    }else{
-        console.log(`Player Loses! ${playerSelection} loses against ${computerSelection}`)
-        return 2;
-
-    }
-}
+//no longer used
+//playing 5 games
+//game();
 
 //function that plays the round of games and keeps score
 //default 5 games
@@ -84,6 +114,9 @@ function game(numberOfGames = 5){
 
     let playerScore = 0;
     let computerScore = 0;
+
+    //Adding event listeners now that we are playing
+    
 
     //plays as many rounds as requested and
     for(let i = 0; i < numberOfGames; i ++){
@@ -97,7 +130,23 @@ function game(numberOfGames = 5){
         
         console.log(`The score is: \nPlayer: ${playerScore} \nComputer: ${computerScore}`)
     }
+
+    
 }
-//playing 5 games
-game();
+
+
+//we do a prompt to ask user for selection
+// function getPlayerSelection(){
+    //     let playerSelection = "";
+
+//     playerSelection = prompt("Choose either rock, paper or scissors >>");
+
+
+//     return playerSelection.toLowerCase().trim();
+// }
+
+// console.log(getPlayerSelection());
+
+
+
 
